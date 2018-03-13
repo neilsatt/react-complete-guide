6c4b8bee38008186f751aef467e1daf7b0447f93
neilsatt <neilsatt@gmail.com>
2018-03-12 20:49:24 -0600

    updated handler

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Bill', age: 23 },
-      { name: 'frank', age: 55 }
+      { id: 1, name: 'Bill', age: 23 }, 
+      { id: 2, name: 'frank', age: 55 }
     ],
     showPersons: false
   }
@@ -20,14 +20,22 @@ class App extends Component {
   // 
 
  // for input field
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-     {name: 'Max', age: 300 },
-     {name: event.target.value, age: 300 },
-     {name: 'Alice', age: 30 },
-    ] 
-   })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+     });
+
+     const person = {
+        ...this.state.persons[personIndex]
+     };
+
+     person.name = event.target.value;
+
+     const persons = [...this.state.persons];
+     persons[personIndex] = person;
+    
+    this.setState({ persons: persons} )
+
   }
 
   deletePersonHandler= (personIndex) => {
@@ -69,7 +77,9 @@ class App extends Component {
                 return <Person 
                 click={() => this.deletePersonHandler(index)}
                 name={person.name} 
-                age={person.age}/>
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}/>
               })}
         </div>
       );
