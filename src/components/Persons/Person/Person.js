a3315f31553f8d914fd293cd927d44163d8b256e
neilsatt <neilsatt@gmail.com>
2018-03-20 19:17:02 -0600

    update to stateful components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component } from 'react';
 import classes from './Person.css';
 /*
   props give you access to the attributes added to </Person> components
@@ -10,15 +10,18 @@ import classes from './Person.css';
   Add input so we can add our own new name. Method comes
   from the App.js file
 */
-const person = (props) => {
+// convert to stateful 
+class Person extends Component {
+  render() {
     return (
-        <div className={classes.Person}>
-          <p onClick={props.click}>I am {props.name} and I'm {props.age} years old</p>
-          <p>{props.children}</p>
-          <input type="text" onChange={props.changed} value={props.name}/>
-        </div>
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>I am {this.props.name} and I'm {this.props.age} years old</p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name}/>
+      </div>
     )
-  
-};
+  }
+}
 
-export default person;
+
+export default Person;
